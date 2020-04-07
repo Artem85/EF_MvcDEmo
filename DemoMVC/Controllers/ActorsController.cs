fc95dffe288b2060abbf1cb88e23d13870241bdd
Zainchkovskyy Artem <zainchkovskyy.artem@gmail.com>
2020-04-08 02:14:42 +0300

    Initial commit

---
+++ DemoMVC/Controllers/ActorsController.cs
@@ -0,0 +1,38 @@
+﻿using DemoConsoleMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DemoConsoleMVC.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private ActorDbContext _context;
+        public ActorsController(ActorDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Actors.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Actors actors)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Actors.Add(actors);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(actors);
+        }
+    }
+}
---
+++ DemoMVC/Models/Actors.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DemoConsoleMVC.Models
+{
+    public partial class Actors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool AcademyWinner { get; set; }
+    }
+}
